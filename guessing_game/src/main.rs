8b71d644f8628d87ac777ab45b806c53580c2cbb
Abdoul Karim <karimniang9@gmail.com>
2024-11-22 07:41:26 +0100

    Guess Game: casting, comparing, error handling
    - Casting to int for comparing the number
    - loop for multiple attemps and break once won

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,42 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Guess the number!");
+    
+    println!("Secret Number");
 
-    println!("Please input the guess.");
+    let secret_number: u32 = rand::thread_rng().gen_range(0..100);
 
-    let mut guess = String::new();
+    println!("The secret number is: {secret_number}");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Guess the number!");
 
-    println!("You guessed {}", guess);
+        println!("Please input the guess.");
+
+        let mut guess = String::new();
+
+        
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+    
+        println!("You guessed {}", guess);
+
+
+        match guess.cmp(&secret_number) { 
+            Ordering::Less => println!("The secret number is greater"),
+            Ordering::Greater => println!("The secret number is lower"),
+            Ordering::Equal => {
+                println!("You won!");
+                break
+            }
+        };  
+    }
 }
