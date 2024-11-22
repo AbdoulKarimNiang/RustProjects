27c0e97e59f5f33ceb07ecc0b8ae48780dd4c8ee
Abdoul Karim <karimniang9@gmail.com>
2024-11-22 07:52:17 +0100

    Simple variable mutability script

---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\karim.niang\Desktop\Personal\VsPythonCode\RustProjects\variables\target\debug\variables.exe: C:\Users\karim.niang\Desktop\Personal\VsPythonCode\RustProjects\variables\src\main.rs
